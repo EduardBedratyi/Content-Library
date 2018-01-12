e776a5965053f0ec474c02ec09126125b1fb44b7
Eduard <0675060050@mail.ru>
2018-01-12 22:59:46 +0200

    changes for correct working of pagination

--- js/content.js
+++ js/content.js
@@ -3,33 +3,36 @@ jQuery(document).ready(function () {
     $('#searchForm').submit( function (e) {
         e.preventDefault();
         var item = $('.search-item').val();
-        $("p:contains(" + item + ")").css("background-color", "#12213a");
+        $("p:contains(" + item + ")").css("background-color", "#91b6cc");
     });
     /*----------------------------o u t p u t   f u n c t i o n s---------------------------*/
     function html_creator_content(array){
         $('.page').html('');
         for(var k in array){
             var div = '';
+            div += '<div class="page-block">';
             div += '<div data-num=1 class="num">';
-            div += '<h2>'+array[k].category+'</h2>';
+            div += '<p>'+'<em>'+'Category:  '+'</em>'+array[k].category+'</p>';
             div += '</div>';
             div += '<div data-num=2 class="num">';
-            div += '<p>'+array[k].userName+'</p>';
+            div += '<p>'+'<em>'+'User name:  '+'</em>'+array[k].userName+'</p>';
             div += '</div>';
             div += '<div data-num=3 class="num">';
-            div += '<p>'+array[k].email+'</p>';
+            div += '<p>'+'<em>'+'User email:  '+'</em>'+array[k].email+'</p>';
             div += '</div>';
             div += '<div data-num=4 class="num">';
-            div += '<p>'+array[k].country+'</p>';
+            div += '<p>'+'<em>'+'User country:  '+'</em>'+array[k].country+'</p>';
             div += '</div>';
             div += '<div  data-num=5 class="num">';
-            div += '<p>'+array[k].file+'</p>';
+            div += '<p>'+'<em>'+'File path:  '+'</em>'+array[k].file+'</p>';
             div += '</div>';
             div += '<div  data-num=6 class="num">';
-            div += '<p>'+array[k].contentType+'</p>';
+            div += '<p>'+'<em>'+'Content type:  '+'</em>'+array[k].contentType+'</p>';
             div += '</div>';
             div += '<div  data-num=7 class="num">';
-            div += '<p>'+array[k].textContent+'</p>';
+            div += '<p>'+'<em>'+'Text content:  '+'</em>'+array[k].textContent+'</p>';
+            div += '</div>';
+            div += '<hr>';
             div += '</div>';
             $('.page').append(div);
         }
@@ -41,8 +44,9 @@ jQuery(document).ready(function () {
         for(var k in array){
             var div = '';
             div += '<div data-num=1 class="num">';
-            div += '<p>'+array[k].userName+'</p>';
+            div += '<p>'+'<em>'+'User name:  '+'</em>' +array[k].userName+'</p>';
             div += '</div>';
+            div += '<hr>';
             $('.page').append(div);
         }
         showPage(array);
@@ -52,8 +56,9 @@ jQuery(document).ready(function () {
         for(var k in array){
             var div = '';
             div += '<div data-num=1 class="num">';
-            div += '<h2>'+array[k].category+'</h2>';
+            div += '<p>'+'<em>'+'Category:  '+'</em>'+array[k].category+'</p>';
             div += '</div>';
+            div += '<hr>';
             $('.page').append(div);
         }
         showPage(array);
@@ -63,8 +68,9 @@ jQuery(document).ready(function () {
         for(var k in array){
             var div = '';
             div += '<div data-num=1 class="num">';
-            div += '<p>'+array[k].file+'</p>';
+            div += '<p>'+'<em>'+'File path:  '+'</em>'+array[k].file+'</p>';
             div += '</div>';
+            div += '<hr>';
             $('.page').append(div);
         }
         showPage(array);
@@ -81,12 +87,12 @@ jQuery(document).ready(function () {
         /*var page = "<span>&laquo;</span>";*/
         for (var i = 0; i < cnt_page; i++) {
             page += "<span data-page=" + i * cnt + "  id=\"page" + (i + 1) + "\">" + (i + 1) + "</span>";/*
-            divide the entire list of pages, for example, 5 records per page and assigning each page its own id and the number (i + 1)*/
+             divide the entire list of pages, for example, 5 records per page and assigning each page its own id and the number (i + 1)*/
         }
         paginator.innerHTML = page;
 
 //first 7 entries {cnt}
-        var div_num = document.querySelectorAll(".num");
+        var div_num = document.querySelectorAll(".page-block");
         for (var i = 0; i < div_num.length; i++) {
             if (i < cnt) {
                 div_num[i].style.display = "block";
@@ -95,33 +101,36 @@ jQuery(document).ready(function () {
         /*set class "paginator_active" to first page*/
         var main_page = document.getElementById("page1");
         main_page.classList.add("paginator_active");
-    };
 
-    /*listing function*/
-    $('#pagination').click(function (event) {
-        var e = event || window.event;
-        var target = e.target;
-        var id = target.id;
+        /*listing function*/
+        pagination_procesing = function (event) {
+            var e = event || window.event;
+            var target = e.target;
+            var id = target.id;
 
-        if (target.tagName.toLowerCase() !== "span") return;
+            if (target.tagName.toLowerCase() !== "span") return;
 
-        var num_ = id.substr(4);
-        var data_page = +target.dataset.page;
-        main_page.classList.remove("paginator_active");
-        main_page = document.getElementById(id);
-        main_page.classList.add("paginator_active");
+            /*var num_ = id.substr(4);*/
+            var data_page = +target.dataset.page;
+            main_page.classList.remove("paginator_active");
+            main_page = document.getElementById(id);
+            main_page.classList.add("paginator_active");
 
-        var j = 0;
-        for (var i = 0; i < div_num.length; i++) {
-            var data_num = div_num[i].dataset.num;
-            if (data_num <= data_page || data_num >= data_page)
-                div_num[i].style.display = "none";
+            var j = 0;
 
+            $(div_num).css('display','none');
+            for (var i = data_page; i < div_num.length; i++) {
+                if (j >= cnt) break;
+                div_num[i].style.display = "block";
+                j++;
+            }
         }
-        for (var i = data_page; i < div_num.length; i++) {
-            if (j >= cnt) break;
-            div_num[i].style.display = "block";
-            j++;
+    };
+    var pagination_procesing;
+    /*click listing function*/
+    $('#pagination').click(function (event) {
+        if(pagination_procesing){
+            pagination_procesing(event);
         }
     });
     /* -----------------------DISCOVER ALL VIDEO AUDIO TEXT---------------------*/
@@ -140,7 +149,8 @@ jQuery(document).ready(function () {
 
     var discoverAll = get("all");
     if(discoverAll.addEventListener){
-        discoverAll.addEventListener("click", function() {
+        discoverAll.addEventListener("click", function(e) {
+            e.preventDefault();
             var new_array = [];
             for (var n in window.content) {
                 for (var k in window.content[n]) {
