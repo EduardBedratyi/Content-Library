b6f4e4cac30478d46e3b2d033ac6aab0a96f90e1
Eduard <0675060050@mail.ru>
2018-01-18 11:29:22 +0200

    litle changes

--- js/addContent.js
+++ js/addContent.js
@@ -40,7 +40,43 @@ jQuery(document).ready(function () {
             if (contentType === undefined){
                 alert('unknown type of file');
             }
+
         });
+            /*----------------------------------ERROR and validate function----------------------*/
+            $('#addContent-text-submit').on('click', function (e) {
+                e.preventDefault();
+
+            function showError(container, errorMessage) {
+                container.className = 'error';
+                var msgElem = document.createElement('span');
+                msgElem.className = "error-message";
+                msgElem.innerHTML = errorMessage;
+                container.appendChild(msgElem);
+            }
+
+            function resetError(container) {
+                container.className = '';
+                if (container.lastChild.className === "error-message") {
+                    container.removeChild(container.lastChild);
+                }
+            }
+
+            function validate(addContent) {
+                var elems = addContent.elements;
+
+                resetError(elems.userName.parentNode);
+                if (!elems.userName.value) {
+                    showError(elems.userName.parentNode, ' Please, enter your Name or nickName');
+                }
+                resetError(elems.message.parentNode);
+                if (!elems.message.value) {
+                    showError(elems.message.parentNode, ' Text is missing');
+                }
+
+            }
+            });
+
+    document.getElementById("addContent-text-submit").addEventListener('click',validate());
         /*-------------------checking if file`s type is available*/
         function extCheck(param, array) {
             return array.indexOf(param) !== -1;
